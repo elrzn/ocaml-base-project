6aba3becd550fc4630c887d95752107c3f201a38
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-06-13 17:39:45 +0200

    Add factorial to example

--- main.ml
+++ main.ml
@@ -1,3 +1,12 @@
 open Base
 
+let factorial x =
+  let rec loop acc rem =
+    match rem with 0 -> acc | _ -> loop (acc * rem) (rem - 1)
+  in
+  loop 1 x
+
+
 let () = "Hello, World!" |> Stdio.print_endline
+
+let () = 5 |> factorial |> Int.to_string |> Stdio.print_endline
